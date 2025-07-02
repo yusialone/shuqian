014a70e3ceb32e43eba9611e66f29c7b9835d9f3
yusialone <yusialone@gmail.com>
2025-07-02 12:28:20 +0800

    fix: remove prefix from InputProps

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,13 +2,13 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  prefix?: React.ReactNode
+  addonPrefix?: React.ReactNode
 }
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, prefix, ...props }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, addonPrefix, ...props }, ref) => {
   return (
     <div className={cn("flex items-center border rounded-md px-3 py-2 bg-background", className)}>
-      {prefix && <span className="mr-2 text-muted-foreground">{prefix}</span>}
+      {addonPrefix && <span className="mr-2 text-muted-foreground">{addonPrefix}</span>}
       <input
         type={type}
         className="flex-1 bg-transparent outline-none"
